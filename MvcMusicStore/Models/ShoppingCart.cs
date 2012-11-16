ae641ae70530f27ac84cc9ad95d7478a7e37a4b2
TimSOS <tsmelser@gmail.com>
2012-11-15 22:49:10 -0500

    Code formatting changes only

--- MvcMusicStore/Models/ShoppingCart.cs
+++ MvcMusicStore/Models/ShoppingCart.cs
@@ -4,224 +4,189 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace MvcMusicStore.Models
-{
-    public partial class ShoppingCart
-    {
-        MusicStoreEntities _db;
-        string ShoppingCartId { get; set; }
+namespace MvcMusicStore.Models {
+	public partial class ShoppingCart {
+		MusicStoreEntities _db;
+		string ShoppingCartId { get; set; }
 
-        public ShoppingCart(MusicStoreEntities db)
-        {
-            _db = db;
-        }
+		public ShoppingCart(MusicStoreEntities db) {
+			_db = db;
+		}
 
-        public const string CartSessionKey = "CartId";
+		public const string CartSessionKey = "CartId";
 
-        public static ShoppingCart GetCart(MusicStoreEntities db, HttpContextBase context)
-        {
-            var cart = new ShoppingCart(db);
-            cart.ShoppingCartId = cart.GetCartId(context);
-            return cart;
-        }
+		public static ShoppingCart GetCart(MusicStoreEntities db, HttpContextBase context) {
+			var cart = new ShoppingCart(db);
+			cart.ShoppingCartId = cart.GetCartId(context);
+			return cart;
+		}
 
-        public static ShoppingCart GetCart(MusicStoreEntities db, HttpContext context)
-        {
-            var cart = new ShoppingCart(db);
-            cart.ShoppingCartId = cart.GetCartId(context);
-            return cart;
-        }
+		public static ShoppingCart GetCart(MusicStoreEntities db, HttpContext context) {
+			var cart = new ShoppingCart(db);
+			cart.ShoppingCartId = cart.GetCartId(context);
+			return cart;
+		}
 
-        // Helper method to simplify shopping cart calls
-        public static ShoppingCart GetCart(MusicStoreEntities db, Controller controller)
-        {
-            return GetCart(db, controller.HttpContext);
-        }
+		// Helper method to simplify shopping cart calls
+		public static ShoppingCart GetCart(MusicStoreEntities db, Controller controller) {
+			return GetCart(db, controller.HttpContext);
+		}
 
-        public void AddToCart(Album album, int quantity)
-        {
-            // Get the matching cart and album instances
-            var cartItem = _db.Carts.SingleOrDefault(
-                c => c.CartId == ShoppingCartId
-                && c.AlbumId == album.AlbumId);
+		public void AddToCart(Album album, int quantity) {
+			// Get the matching cart and album instances
+			var cartItem = _db.Carts.SingleOrDefault(
+				c => c.CartId == ShoppingCartId
+				&& c.AlbumId == album.AlbumId);
 
-            if (cartItem == null)
-            {
-                // Create a new cart item if no cart item exists
-                cartItem = new Cart
-                {
-                    AlbumId = album.AlbumId,
-                    CartId = ShoppingCartId,
-                    Count = quantity,
-                    DateCreated = DateTime.Now
-                };
+			if (cartItem == null) {
+				// Create a new cart item if no cart item exists
+				cartItem = new Cart
+				{
+					AlbumId = album.AlbumId,
+					CartId = ShoppingCartId,
+					Count = quantity,
+					DateCreated = DateTime.Now
+				};
 
-                _db.Carts.Add(cartItem);
-            }
-            else
-            {
-                // If the item does exist in the cart, then add one to the quantity
-                cartItem.Count++;
-            }
-        }
+				_db.Carts.Add(cartItem);
+			} else {
+				// If the item does exist in the cart, then add one to the quantity
+				cartItem.Count++;
+			}
+		}
 
-        public int RemoveFromCart(int id)
-        {
-            // Get the cart
-            var cartItem = _db.Carts.Single(
-                cart => cart.CartId == ShoppingCartId
-                && cart.RecordId == id);
+		public int RemoveFromCart(int id) {
+			// Get the cart
+			var cartItem = _db.Carts.Single(
+				cart => cart.CartId == ShoppingCartId
+				&& cart.RecordId == id);
 
-            int itemCount = 0;
+			int itemCount = 0;
 
-            if (cartItem != null)
-            {
-                if (cartItem.Count > 1)
-                {
-                    cartItem.Count--;
-                    itemCount = cartItem.Count;
-                }
-                else
-                {
-                    _db.Carts.Remove(cartItem);
-                }
+			if (cartItem != null) {
+				if (cartItem.Count > 1) {
+					cartItem.Count--;
+					itemCount = cartItem.Count;
+				} else {
+					_db.Carts.Remove(cartItem);
+				}
 
-            }
+			}
 
-            return itemCount;
-        }
+			return itemCount;
+		}
 
-        public void EmptyCart()
-        {
-            var cartItems = _db.Carts.Where(cart => cart.CartId == ShoppingCartId);
+		public void EmptyCart() {
+			var cartItems = _db.Carts.Where(cart => cart.CartId == ShoppingCartId);
 
-            foreach (var cartItem in cartItems)
-            {
-                _db.Carts.Remove(cartItem);
-            }
+			foreach (var cartItem in cartItems) {
+				_db.Carts.Remove(cartItem);
+			}
 
-        }
+		}
 
-        public List<Cart> GetCartItems()
-        {
-            return _db.Carts.Where(cart => cart.CartId == ShoppingCartId).ToList();
-        }
+		public List<Cart> GetCartItems() {
+			return _db.Carts.Where(cart => cart.CartId == ShoppingCartId).ToList();
+		}
 
-        public int GetCount()
-        {
-            // Get the count of each item in the cart and sum them up
-            int? count = (from cartItems in _db.Carts
-                          where cartItems.CartId == ShoppingCartId
-                          select (int?)cartItems.Count).Sum();
+		public int GetCount() {
+			// Get the count of each item in the cart and sum them up
+			int? count = (from cartItems in _db.Carts
+						  where cartItems.CartId == ShoppingCartId
+						  select (int?)cartItems.Count).Sum();
 
-            // Return 0 if all entries are null
-            return count ?? 0;
-        }
+			// Return 0 if all entries are null
+			return count ?? 0;
+		}
 
-        public decimal GetTotal()
-        {
-            // Multiply album price by count of that album to get 
-            // the current price for each of those albums in the cart
-            // sum all album price totals to get the cart total
-            decimal? total = (from cartItems in _db.Carts
-                              where cartItems.CartId == ShoppingCartId
-                              select (int?)cartItems.Count * cartItems.Album.Price).Sum();
-            return total ?? decimal.Zero;
-        }
+		public decimal GetTotal() {
+			// Multiply album price by count of that album to get 
+			// the current price for each of those albums in the cart
+			// sum all album price totals to get the cart total
+			decimal? total = (from cartItems in _db.Carts
+							  where cartItems.CartId == ShoppingCartId
+							  select (int?)cartItems.Count * cartItems.Album.Price).Sum();
+			return total ?? decimal.Zero;
+		}
 
-        public int CreateOrder(Order order)
-        {
-            var cartItems = GetCartItems();
-            return CreateOrder(order, cartItems);
-        }
+		public int CreateOrder(Order order) {
+			var cartItems = GetCartItems();
+			return CreateOrder(order, cartItems);
+		}
 
-        public int CreateOrder(Order order, List<Cart> cartItems)
-        {
-            decimal orderTotal = 0;
+		public int CreateOrder(Order order, List<Cart> cartItems) {
+			decimal orderTotal = 0;
 
-            // Iterate over the items in the cart, adding the order details for each
-            foreach (var item in cartItems)
-            {
-                var album = _db.Albums.Find(item.AlbumId);
+			// Iterate over the items in the cart, adding the order details for each
+			foreach (var item in cartItems) {
+				var album = _db.Albums.Find(item.AlbumId);
 
-                var orderDetail = new OrderDetail
-                {
-                    AlbumId = item.AlbumId,
-                    OrderId = order.OrderId,
-                    UnitPrice = album.Price,
-                    Quantity = item.Count,
-                };
+				var orderDetail = new OrderDetail
+				{
+					AlbumId = item.AlbumId,
+					OrderId = order.OrderId,
+					UnitPrice = album.Price,
+					Quantity = item.Count,
+				};
 
-                // Set the order total of the shopping cart
-                orderTotal += (item.Count * album.Price);
+				// Set the order total of the shopping cart
+				orderTotal += (item.Count * album.Price);
 
-                _db.OrderDetails.Add(orderDetail);
-            }
+				_db.OrderDetails.Add(orderDetail);
+			}
 
-            // Set the order's total to the orderTotal count
-            order.Total = orderTotal;
+			// Set the order's total to the orderTotal count
+			order.Total = orderTotal;
 
-            // Empty the shopping cart
-            EmptyCart();
+			// Empty the shopping cart
+			EmptyCart();
 
-            // Return the OrderId as the confirmation number
-            return order.OrderId;
-        }
+			// Return the OrderId as the confirmation number
+			return order.OrderId;
+		}
 
-        // We're using HttpContextBase to allow access to cookies.
-        public string GetCartId(HttpContextBase context)
-        {
-            if (context.Session[CartSessionKey] == null)
-            {
-                if (!string.IsNullOrWhiteSpace(context.User.Identity.Name))
-                {
-                    context.Session[CartSessionKey] = context.User.Identity.Name;
-                }
-                else
-                {
-                    // Generate a new random GUID using System.Guid class
-                    Guid tempCartId = Guid.NewGuid();
+		// We're using HttpContextBase to allow access to cookies.
+		public string GetCartId(HttpContextBase context) {
+			if (context.Session[CartSessionKey] == null) {
+				if (!string.IsNullOrWhiteSpace(context.User.Identity.Name)) {
+					context.Session[CartSessionKey] = context.User.Identity.Name;
+				} else {
+					// Generate a new random GUID using System.Guid class
+					Guid tempCartId = Guid.NewGuid();
 
-                    // Send tempCartId back to client as a cookie
-                    context.Session[CartSessionKey] = tempCartId.ToString();
-                }
-            }
+					// Send tempCartId back to client as a cookie
+					context.Session[CartSessionKey] = tempCartId.ToString();
+				}
+			}
 
-            return context.Session[CartSessionKey].ToString();
-        }
+			return context.Session[CartSessionKey].ToString();
+		}
 
-        public string GetCartId(HttpContext context)
-        {
-            if (context.Session[CartSessionKey] == null)
-            {
-                if (!string.IsNullOrWhiteSpace(context.User.Identity.Name))
-                {
-                    context.Session[CartSessionKey] = context.User.Identity.Name;
-                }
-                else
-                {
-                    // Generate a new random GUID using System.Guid class
-                    Guid tempCartId = Guid.NewGuid();
+		public string GetCartId(HttpContext context) {
+			if (context.Session[CartSessionKey] == null) {
+				if (!string.IsNullOrWhiteSpace(context.User.Identity.Name)) {
+					context.Session[CartSessionKey] = context.User.Identity.Name;
+				} else {
+					// Generate a new random GUID using System.Guid class
+					Guid tempCartId = Guid.NewGuid();
 
-                    // Send tempCartId back to client as a cookie
-                    context.Session[CartSessionKey] = tempCartId.ToString();
-                }
-            }
+					// Send tempCartId back to client as a cookie
+					context.Session[CartSessionKey] = tempCartId.ToString();
+				}
+			}
 
-            return context.Session[CartSessionKey].ToString();
-        }
+			return context.Session[CartSessionKey].ToString();
+		}
 
-        // When a user has logged in, migrate their shopping cart to
-        // be associated with their username
-        public void MigrateCart(string userName)
-        {
-            var shoppingCart = _db.Carts.Where(c => c.CartId == ShoppingCartId);
+		// When a user has logged in, migrate their shopping cart to
+		// be associated with their username
+		public void MigrateCart(string userName) {
+			var shoppingCart = _db.Carts.Where(c => c.CartId == ShoppingCartId);
 
-            foreach (Cart item in shoppingCart)
-            {
-                item.CartId = userName;
-            }
+			foreach (Cart item in shoppingCart) {
+				item.CartId = userName;
+			}
 
-        }
-    }
+		}
+	}
 }
